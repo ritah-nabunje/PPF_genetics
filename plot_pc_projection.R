c7668db392fdd89364174d4fdf69843e76830bad
Ritah Nabunje <rn180@leicester.ac.uk>
2024-05-23 16:18:50 +0100

    Update plot_pc_projection.R

--- plot_pc_projection.R
+++ plot_pc_projection.R
@@ -1,1 +1,9 @@
-# Plotting sample PCs on the reference PC loadings
+# adjust study PCs to same scale as the ref.eigenvec
+# to adjust, computedPCs <- projectedPC/(-sqrt(eigenval)/2)
+# make new set of PCs
+basque_cPCs <- data.frame(basque_pcs$superpop)
+names(basque_cPCs)[1]<- "superpop"
+for (i in 1:10){
+  basque_cPCs[1+i] <- basque_pcs[,paste0("PC", i)]/(-sqrt(eigen_vals[i,])/2)
+  names(basque_cPCs)[1+i] <- paste0("PC", i)
+}
